eec523c209e784cf4ccc3bd539f7c300b655202a
TGHeadle1371 <tgheadle@gmail.com>
2019-04-18 10:01:20 -0600

    style

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,10 +4,8 @@ import {Link} from "react-router-dom";
 function Nav() {
     return(
         <nav className="navbar navbar-dark bg-dark text-white">
-            <h2>React Google Book Search </h2>
-            <Link to="/">Home</Link>
-            <br></br> 
-            <Link to="/saved">Saved</Link>
+            <Link to="/"><h2 id="navTitle">React Google Book Search </h2></Link>
+            <Link to="/saved" id="savedLink">Saved</Link>
         </nav>
     );
 }
